67e2aea679a39914aae410ae6ea301ca3f5b2f37
Maria Antônia Amado <48838678+AmadoMaria@users.noreply.github.com>
2023-05-08 19:37:35 -0300

    Limitando eixo x na figura 2 e adicionando plot
    pwelch fazia a exibição em decibéis

--- analise_hipocampo_rato.m
+++ analise_hipocampo_rato.m
@@ -9,6 +9,7 @@ amostra_completo = Dados.Completo(1:length(tt));
 amostra_estado1 = Dados.Estado1(1:length(tt));
 amostra_estado2 = Dados.Estado2(1:length(tt));
 
+figure(1)
 hold on
 plot(tt, amostra_completo)
 plot(tt, amostra_estado1)
@@ -17,9 +18,11 @@ hold off
 
 %%
 % 1) Espectograma do dado "Completo"
-
+figure(2)
 [s,f,t]=spectrogram(Dados.Completo,2*sr,sr/2,[],sr);
 imagesc(t,f,abs(s))
+colorbar
+clim([0 50]) %caxis não é recomendado
 ylim([0 50])
 ylabel('Frequencia (Hz)')
 xlabel('Time (s)')
@@ -29,9 +32,22 @@ axis xy
 % 2) Plotando o PSD dos dois estados na mesma figura
 estado1_filtered = eegfilt(Dados.Estado1, sr, 0, 20);
 estado2_filtered = eegfilt(Dados.Estado2, sr, 0, 20);
+
+figure(3)
 hold on
-pwelch(estado1_filtered,2*sr,sr/2,[],sr);
-pwelch(estado2_filtered,2*sr,sr/2,[],sr);
+[pxx1,f1]=pwelch(estado1_filtered,2*sr,sr/2,[],sr);
+[pxx2,f2]=pwelch(estado2_filtered,2*sr,sr/2,[],sr);
+
+plot(f1, pxx1)
+plot(f2, pxx2)
+set(gca,'XLim',[0 30])
 hold off
 legend('estado1','estado2')
+% O estado1 representa o REM, já que a densidade espectral no intervalo de
+% 4Hz a 12Hz é maior em comparação ao estado2, e de acordo com o artigo
+% (adicionar referência) existe um aumento significativo do theta no REM em
+% comparação com o SWS
+%%
+% 3) 
+
 
